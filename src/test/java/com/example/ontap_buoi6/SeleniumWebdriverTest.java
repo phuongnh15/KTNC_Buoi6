74730fcd18f6187208a067cac8da14f65f0878e8
phuongnh15 <162479809+phuongnh15@users.noreply.github.com>
2025-06-26 15:22:16 +0700

    Update Selenium

--- src/test/java/com/example/ontap_buoi6/SeleniumWebdriverTest.java
+++ src/test/java/com/example/ontap_buoi6/SeleniumWebdriverTest.java
@@ -8,33 +8,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumWebdriverTest {
-    private WebDriver webDriver;
-
-    @BeforeEach
-    void setUp(){
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
-    }
-
-    @Test
-    public void dangNhap() throws InterruptedException {
-        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
-
-        Thread.sleep(2000);
-
-        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
-
-        Thread.sleep(2000);
-
-        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
-
-        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
-        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
-
-        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
-
-        Thread.sleep(2000);
-
-        webDriver.quit();
-    }
+//    private WebDriver webDriver;
+//
+//    @BeforeEach
+//    void setUp(){
+//        WebDriverManager.chromedriver().setup();
+//        webDriver = new ChromeDriver();
+//    }
+//
+//    @Test
+//    public void dangNhap() throws InterruptedException {
+//        webDriver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
+//
+//        Thread.sleep(2000);
+//
+//        webDriver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click();
+//
+//        Thread.sleep(2000);
+//
+//        webDriver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click();
+//
+//        webDriver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa");
+//        webDriver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
+//
+//        webDriver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
+//
+//        Thread.sleep(2000);
+//
+//        webDriver.quit();
+//    }
 }
